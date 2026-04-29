ae2e27ddb7191dcc669fd8e2cd3688b57302144b
dxvidssss <105018918+dxvidssss@users.noreply.github.com>
2026-04-29 12:00:51 +0200

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -23,7 +23,7 @@ local Config = {
     Secret          = "WeTheBestMusic", -- This makes the script ONLY run from the key script. Even if they copy the original obfuscated script to bypass the key, they won't be able to!
     
     -- [3] Scripts & Links
-    MainScriptURL   = "https://raw.githubusercontent.com/dxvidssss/keysystem/refs/heads/main/zqyr.lua", -- The raw URL of your main script
+    MainScriptURL   = "https://raw.githubusercontent.com/dxvidssss/keysystem/refs/heads/main/script.lua", -- The raw URL of your main script
     
     -- [4] Social Media Settings (Set to true to show, false to hide)
     ShowDiscord     = false,
